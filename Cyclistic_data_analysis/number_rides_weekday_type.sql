d3b44a53dfc487fa0a315176689a76d21f549ff4
hslincoln <62722888+hslincoln@users.noreply.github.com>
2022-03-30 10:48:53 -0400

    Update number_rides_weekday_type.sql

--- Cyclistic_data_analysis/number_rides_weekday_type.sql
+++ Cyclistic_data_analysis/number_rides_weekday_type.sql
@@ -1,14 +1,13 @@
 --Number of rides by weekday between members and casual riders
 SELECT *
 FROM (SELECT 
-			ride_id,
-			day_of_week,
+	    ride_id,
+	    day_of_week,
             member_casual
-			FROM [Cyclistic].[dbo].[all_data]
+       FROM [Cyclistic].[dbo].[all_data]
 	) AS t
 	PIVOT (COUNT(ride_id)
-			FOR day_of_week IN (
-            [Sunday],[Monday],[Tuesday],[Wednesday],
-            [Thursday],[Friday],[Saturday]
-			)
-    ) AS pvt_tbl;
+		FOR day_of_week IN (
+                [Sunday],[Monday],[Tuesday],[Wednesday],
+                [Thursday],[Friday],[Saturday])
+        ) AS pvt_tbl;
