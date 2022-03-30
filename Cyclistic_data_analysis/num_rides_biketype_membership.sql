23ece826f54beb28b80862dee2925882d4b4cfa2
hslincoln <62722888+hslincoln@users.noreply.github.com>
2022-03-30 10:44:56 -0400

    Update num_rides_biketype_membership.sql

--- Cyclistic_data_analysis/num_rides_biketype_membership.sql
+++ Cyclistic_data_analysis/num_rides_biketype_membership.sql
@@ -1,7 +1,7 @@
 --Number of rides by Bike Type and Membership Type
 SELECT rideable_type, 
        member_casual, 
-	   COUNT(*) AS num_rides
+       COUNT(*) AS num_rides
 FROM [Cyclistic].[dbo].[all_data]
 GROUP BY rideable_type, member_casual
 ORDER BY rideable_type, member_casual
