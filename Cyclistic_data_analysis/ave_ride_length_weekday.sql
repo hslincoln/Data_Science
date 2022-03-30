23930cab68e203a35e201f4679a1c5780d5991c7
hslincoln <62722888+hslincoln@users.noreply.github.com>
2022-03-30 10:44:05 -0400

    Update ave_ride_length_weekday.sql

--- Cyclistic_data_analysis/ave_ride_length_weekday.sql
+++ Cyclistic_data_analysis/ave_ride_length_weekday.sql
@@ -1,13 +1,13 @@
 --Average Ride Length for Weekday
 SELECT *
 FROM (SELECT 
-			day_of_week,
+	    day_of_week,
             member_casual,
-			ride_length
-		FROM [Cyclistic].[dbo].[all_data]
-	) AS t
-	PIVOT (AVG(ride_length)
-			FOR day_of_week IN (
-            [Sunday],[Monday],[Tuesday],[Wednesday],
-            [Thursday],[Friday],[Saturday])
-    ) AS pvt_tbl;
+	    ride_length
+      FROM [Cyclistic].[dbo].[all_data]
+      ) AS t
+      PIVOT (AVG(ride_length)
+	      FOR day_of_week IN (
+              [Sunday],[Monday],[Tuesday],[Wednesday],
+              [Thursday],[Friday],[Saturday])
+      ) AS pvt_tbl;
