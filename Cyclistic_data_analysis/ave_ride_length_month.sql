5a998fcfc0aa4854ab55de603f480ddf3fb8c2f4
hslincoln <62722888+hslincoln@users.noreply.github.com>
2022-03-30 10:41:58 -0400

    Update ave_ride_length_month.sql

--- Cyclistic_data_analysis/ave_ride_length_month.sql
+++ Cyclistic_data_analysis/ave_ride_length_month.sql
@@ -1,14 +1,14 @@
 --Average ride length for each month
 SELECT *
 FROM (SELECT 
-			ride_month_text,
+	    ride_month_text,
             member_casual,
-			ride_length
-		FROM [Cyclistic].[dbo].[all_data]
-	) AS t
-	PIVOT (AVG(ride_length)
-			FOR ride_month_text IN (
-            [January],[February],[March],
-            [April],[May],[June],[July],[August],
-			[September],[October],[November],[December])
-    ) AS pvt_tbl;
+	    ride_length
+      FROM [Cyclistic].[dbo].[all_data]
+      ) AS t
+      PIVOT (AVG(ride_length)
+	      FOR ride_month_text IN (
+              [January],[February],[March],
+              [April],[May],[June],[July],[August],
+	      [September],[October],[November],[December])
+       ) AS pvt_tbl;
